e690a670d922f64626c80156c3fba1679251b8b2
unknown <dmc00004@gmail.com>
2024-03-27 21:37:22 +0300

    update distance.py

--- sauvc_navigation/distance.py
+++ sauvc_navigation/distance.py
@@ -1,6 +1,16 @@
 
 import math
 
+class ObjectDataSegment :
+    def __init__(self, width, height, maxRatio, badR, goodR, p, color):
+        self.color = color
+        self.goodR = goodR
+        self.badR = badR
+        self.maxRatio = maxRatio
+        self.height = height
+        self.width = width
+        self.p = p
+
 class DistanceCalculator:
     # Y, X
     def __init__(self,
@@ -21,7 +31,12 @@ class DistanceCalculator:
     def calcDistanceAndAngle(self, xyxy, cls) :
 
         if cls in self.object_attrs:
-            realSizeX, realSizeY = self.object_attrs[cls][0], self.object_attrs[cls][1]
+
+            if cls == 'gate_qualification': cls = 'gate'
+
+            if cls.p < self.object_attrs[cls].p: return None, None
+
+            realSizeX, realSizeY = self.object_attrs[cls].width, self.object_attrs[cls].height
 
             sizeX = (xyxy[2] - xyxy[0]) / self.imgsz[1]
             sizeY = (xyxy[3] - xyxy[1]) / self.imgsz[0]
@@ -31,13 +46,22 @@ class DistanceCalculator:
             DX = self._calcDistance(angleSizeX, realSizeX)
             DY = self._calcDistance(angleSizeY, realSizeY)
 
+            calcRatio = sizeX / sizeY
+            realRatio = realSizeX / realSizeY
+            if (max(calcRatio, realRatio) / min(calcRatio, realRatio)) > self.object_attrs[cls].maxRatio:
+                return None, None
+
             D = DX
-            if (sizeY != 0 and sizeY != 1): D = DY
-            if (sizeX != 0 and sizeX != 1): D = DX
-            if (sizeX != 0 and sizeX != 1 and sizeY != 0 and sizeY != 1) : D = (DX + DY) / 2
+            if (xyxy[1] == 0 or xyxy[3] == self.imgsz[0]) and (xyxy[0] == 0 or xyxy[2] == self.imgsz[1]): return None, None
+            if (xyxy[0] == 0 or xyxy[2] == self.imgsz[1]): return None, None
+            if (xyxy[0] != 0 and xyxy[2] != self.imgsz[1]): D = DX
+            if (xyxy[1] != 0 and xyxy[3] != self.imgsz[0]): D = DY
+            if (xyxy[0] != 0 and xyxy[2] != self.imgsz[1]) and (xyxy[1] != 0 and xyxy[3] != self.imgsz[0]): D = (DX + DY) / 2
 
             positionX = xyxy[0] + (xyxy[2] - xyxy[0]) / 2
             angleX = (positionX / self.imgsz[1]) * self.fov[1]
             angle = angleX - self.fov[1] / 2
 
             return D, angle
+
+        return None, None
